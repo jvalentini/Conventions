1938cc6e1469e2cbe81bd01a4eb23081622ee933
Justin Valentini <justin.valentini@gmail.com>
2013-03-05 03:14:38 -0500

    Implement generic handling of resourceful controllers

--- app/controllers/convention_controller.rb
+++ app/controllers/convention_controller.rb
@@ -1,82 +1,97 @@
 class ConventionController < ApplicationController
-  # GET /products
-  # GET /products.json
+
+  def initialize
+    @resource_collection = self.controller_name
+    @resource_name       = self.controller_name.singularize
+    @resource_symbol     = @resource_name.to_sym
+    @resource            = @resource_name.capitalize.constantize.new
+
+    @resource_class_name = @resource.class.name.downcase
+    require @resource_class_name
+  end
+
+  def resource_url
+    self.send((@resource_collection + '_url').to_sym)
+  end
+
+  # GET /@resource
+  # GET /@resource.json
   def index
-    @products = Product.all
+    @collection = @resource.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @products }
+      format.json { render json: @collection }
     end
   end
 
-  # GET /products/1
-  # GET /products/1.json
+  # GET /@resource/1
+  # GET /@resource/1.json
   def show
-    @product = Product.find(params[:id])
+    @member = @resource.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @product }
+      format.json { render json: @member }
     end
   end
 
-  # GET /products/new
-  # GET /products/new.json
+  # GET /@resource/new
+  # GET /@resource/new.json
   def new
-    @product = Product.new
+    @member = @resource.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @product }
+      format.json { render json: @member }
     end
   end
 
-  # GET /products/1/edit
+  # GET /@resource/1/edit
   def edit
-    @product = Product.find(params[:id])
+    @member = @resource.find(params[:id])
   end
 
-  # POST /products
-  # POST /products.json
+  # POST /@resource
+  # POST /@resource.json
   def create
-    @product = Product.new(params[:product])
+    @member = @resource.new(params[@resource_symbol])
 
     respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render json: @product, status: :created, location: @product }
+      if @member.save
+        format.html { redirect_to @member, notice: '@resource was successfully created.' }
+        format.json { render json: @member, status: :created, location: @member }
       else
         format.html { render action: "new" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @member.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /products/1
-  # PUT /products/1.json
+  # PUT /@resource/1
+  # PUT /@resource/1.json
   def update
-    @product = Product.find(params[:id])
+    @member = @resource.find(params[:id])
 
     respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+      if @member.update_attributes(params[@resource_symbol])
+        format.html { redirect_to @member, notice: '@resource was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @member.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
+  # DELETE /@resource/1
+  # DELETE /@resource/1.json
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+    @member = @resource.find(params[:id])
+    @member.destroy
 
     respond_to do |format|
-      format.html { redirect_to products_url }
+      format.html { redirect_to resource_url }
       format.json { head :no_content }
     end
   end
